33834e80424a0debdf14c5af9460299769b0ce97
Jonathan Garcia <jo-pg@hotmail.com>
2022-01-15 17:55:20 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/devsuperior/dsvomie/DsvomieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvomieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvomieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvomie/DsvomieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
